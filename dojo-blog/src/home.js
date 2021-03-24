8a88a68badef76f0e8bf2d86eea302b155ce487c
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-03-24 15:36:11 -0300

    Click events

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Hello, Ninjas!',e.target)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Hello, ' + name)
+    console.log('Target: ', e.target)
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={(e) => handleClickAgain('Manuel',e)}>Click me again!</button>
     </div>
   );
 }
