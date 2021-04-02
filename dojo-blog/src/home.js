00a6111f5a6b7a84528537c5a7663c3ab4cc207d
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-02 12:45:19 -0300

    Fetching data with useEffect

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -4,33 +4,30 @@ import { useState, useEffect } from 'react'
 import BlogList from './blogList'
  
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Luigi', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario')
-
-  const handleDelete = id => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+  const [blogs, setBlogs] = useState(null)
 
   // This function is fired every time the DOM is rendered.
   // To prevent this behavior, we can add a "dependecies array".
   // It can be an empty array or we can add real dependencies.
   // The only dependencie we gonna have is "name".
   useEffect(() => {
-    console.log('using useEffect hook')
-    console.log(name)
-  }, [name])
+    // Fetching the data from the JSON server
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data)
+      })
+  },[])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>Change name!</button>
-      <p>{ name }</p>
+      {/*If blogs evaluate to false, nothing is outputed on the screen, because the right
+      side of && operator is not evaluated.
+      If blogs evaluate to true, then the right side of the && operator is evaluated and
+      then the BlogList is rendered on the screen.*/}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 }
