8f3e47053ff3db19c9e1bad9343826f37da3380e
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-03 10:39:16 -0300

    Conditional loading message

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -5,24 +5,29 @@ import BlogList from './blogList'
  
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
   // This function is fired every time the DOM is rendered.
   // To prevent this behavior, we can add a "dependecies array".
   // It can be an empty array or we can add real dependencies.
   // The only dependencie we gonna have is "name".
   useEffect(() => {
-    // Fetching the data from the JSON server
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      // Fetching the data from the JSON server
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json()
       })
       .then(data => {
         setBlogs(data)
+        setIsPending(false)
       })
+    }, 1000)
   },[])
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {/*If blogs evaluate to false, nothing is outputed on the screen, because the right
       side of && operator is not evaluated.
       If blogs evaluate to true, then the right side of the && operator is evaluated and
