5bd3ab7fc6215dd768c24a150fdafc30216fbc7d
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-05 19:44:33 -0300

    Handling fetch errors

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -6,6 +6,7 @@ import BlogList from './blogList'
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   // This function is fired every time the DOM is rendered.
   // To prevent this behavior, we can add a "dependecies array".
@@ -16,10 +17,17 @@ const Home = () => {
       // Fetching the data from the JSON server
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        // This sets the "message" propertie that will be catched by the error (if it happens).
+        if(!res.ok) throw Error('Could not fetch the data for that resource!')
         return res.json()
       })
       .then(data => {
         setBlogs(data)
+        setIsPending(false)
+        setError(null)
+      })
+      .catch(err => {
+        setError(err.message)
         setIsPending(false)
       })
     }, 1000)
@@ -27,12 +35,13 @@ const Home = () => {
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
       {/*If blogs evaluate to false, nothing is outputed on the screen, because the right
       side of && operator is not evaluated.
       If blogs evaluate to true, then the right side of the && operator is evaluated and
       then the BlogList is rendered on the screen.*/}
-      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      { blogs && <BlogList blogs={blogs} title="All blogs" /> }
     </div>
   );
 }
