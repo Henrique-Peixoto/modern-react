386195235f299b6208bf8211cfe505a422079a4e
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-10 11:25:32 -0300

    Exact match routes

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './navbar'
 import Home from './home'
+import Create from './Create'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -8,9 +9,18 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
+          {/**When the first route is matched, the matched route returns
+           * If we don't use the Switch component, all the matched routes will be returned
+           */}
           <Switch>
-            <Route path="/">
+            {/**The "exact" attribute allows that only the exact path we want to be matched
+             * notice that "/" matches with "/create", but with "exact", it doesn't happen anymore
+            */}
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
