15d6c5866f89423ca1008746d8f3af861938ed57
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-18 21:49:18 -0300

    Reusing custom hooks

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from 'react-router-dom'
+import useFecth from './useFetch'
 
 const BlogDetails = () => {
   // This allows us to grab parameters from the URL
   // We want the ":id" in the route that leads to a blog
   const { id } = useParams()
+  const { data:blog, error, isPending} = useFecth('http://localhost:8000/blogs/' + id)
 
   return (
     <div className="blog-detail">
-      <h2>Blog Details - { id }</h2>
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article className="blog-content">
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
     </div>
   )
 }
--- dojo-blog/src/navbar.js
+++ dojo-blog/src/navbar.js
@@ -6,17 +6,21 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <Link to="/">
+        <h1>The Dojo Blog</h1>
+      </Link>
       <div className="links">
         {/* The value of "to" is matched against the paths in our Router tags (App.js)
          If a value matches, the content inside the <Router>...</Router> is returned,
          that's why there is no need for requesting the page to the server.*/}
-        <Link to="/">Home</Link>
-        <Link to="/create" style={{
-          color: 'white',
-          backgroundColor: '#f1356d',
-          borderRadius: '8px'
-        }}>New Blog</Link>
+        <div className="navbar-button">
+          <Link to="/">Home</Link>
+          <Link to="/create" style={{
+            color: 'white',
+            backgroundColor: '#f1356d',
+            borderRadius: '8px'
+          }}>New Blog</Link>
+        </div> 
       </div>
     </nav>
   );
