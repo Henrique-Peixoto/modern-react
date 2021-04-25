f68e3d8689f6af9548e1b79b63d212e4941b6ba5
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-25 13:11:11 -0300

    Deleting blogs

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import useFecth from './useFetch'
 
 const BlogDetails = () => {
@@ -6,6 +6,15 @@ const BlogDetails = () => {
   // We want the ":id" in the route that leads to a blog
   const { id } = useParams()
   const { data:blog, error, isPending} = useFecth(`http://localhost:8000/blogs/${id}`)
+  const history = useHistory()
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + id,{
+      method: 'DELETE'
+    }).then(() => {
+      history.push("/")
+    })
+  }
 
   return (
     <div className="blog-detail">
@@ -16,6 +25,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
