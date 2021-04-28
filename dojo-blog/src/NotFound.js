87ea871b0d5a977a4584afec310871521c2401e8
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-04-27 22:04:00 -0300

    404 style

--- dojo-blog/src/NotFound.js
+++ dojo-blog/src/NotFound.js
@@ -2,10 +2,17 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Sorry</h2>
-      <p>That page cannot be found</p>
-      <Link to="/">Back to the homepage</Link>
+    <div className="nf-wrapper">
+      <div className="nf-title-message">
+        <div className="nf-title">
+          <span className="nf-number">404</span>
+          <span className="nf-subtitle">not found</span>
+        </div>
+        <div className="nf-message">Sorry, the page you were looking for could not be found!</div>
+      </div>
+      <div className="nf-homepage">
+        <Link to="/">Back to the homepage</Link>
+      </div>
     </div>
   )
 }
